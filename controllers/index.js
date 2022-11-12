38b4a8d66f549527e2bd88a7ae1ca5579dfa0acc
Vincent Wizley <117470535+Vincley@users.noreply.github.com>
2022-11-12 12:31:03 +0700

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,13 @@ const produk = require('./controller_produk')
 const supplier = require('./controller_supplier')
 const pengangkutan = require('./controller_pengangkutan')
 const konsumer = require('./controller_konsumer')
+const _user = require('./controller_user')
 
 module.exports = {
     ekspedisi,
     produk,
     supplier,
     pengangkutan,
-    konsumer
+    konsumer,
+    _user
 };
